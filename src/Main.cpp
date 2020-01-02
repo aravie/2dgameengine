f6dba5db636a57d8903260eb33e6929e80f09763
aravie <raviee@gmx.de>
2020-01-02 11:39:19 -0800

    Creating a Game Loop (brain map sequence)

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
 
 int main(int argc, char *argv[]){
-	//TODO:
-	//Game *game = new Game();
+	Game *game = new Game();
+	
+	game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);	
 
-	std::cout << "Game is running..." << std::endl;
+	while(game->IsRunning()){
+		game->ProcessInput();
+		game->Update();
+		game->Render();
+	}
+
+	game->Destroy();	
+
 	return 0;
 }
