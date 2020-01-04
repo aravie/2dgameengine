1b625d8bac4962f9a584a8cbd91d208266b9221f
aravie <raviee@gmx.de>
2020-01-04 05:20:32 -0800

    Fixing Time Step & work with GLM vector2 in Game.cpp, creating Entity and component prototypes

---
+++ src/Entity.cpp
@@ -0,0 +1,31 @@
+#include "./Entity.h"
+
+// constructor with members instantiation
+Entity::Entity(EntityManager& manager): manager(manager) {
+	this->isActive = true;
+}
+
+Entity::Entity(EntityManager& manager, std::string name): manager(manager), name(name) {
+	this->isActive = true;
+}
+
+void Entity::Update(float deltaTime){
+	//looping the list of components
+	for(auto& component: components) {
+		component->Update(deltaTime);
+	}
+}
+
+void Entity::Render(){
+	for(auto& component: components){
+		component->Render();
+	}
+}
+
+void Entity::Destroy(){
+	this->isActive = false;
+}
+
+bool Entity::IsActive() const{
+	return this->isActive;
+}
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "./Constants.h"
 #include "./Game.h"
+#include "../lib/glm/glm.hpp"
+
+EntityManager manager;
+SDL_Renderer* Game::renderer;
 
 Game::Game() {
 	this->isRunning = false;
@@ -14,10 +18,8 @@ bool Game::IsRunning() const {
 	return this->isRunning;
 }
 
-float projectilePosX = 0.0f;
-float projectilePosY = 0.0f;
-float projectileVelX = 10.0f;
-float projectileVelY = 10.0f;
+glm::vec2 projectilePos = glm::vec2(0.0f, 0.0f);
+glm::vec2 projectileVel = glm::vec2(20.0f, 20.0f);
 
 void Game::Initialize(int width, int height) {
 	if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
@@ -65,9 +67,23 @@ void Game::ProcessInput() {
 	}	
 }
 
-void Game::Update() {	
-	projectilePosX += projectileVelX;
-	projectilePosY += projectileVelY;
+void Game::Update() {
+	// Wait until 16ms has elapsed since the last frame
+	while(!SDL_TICKS_PASSED(SDL_GetTicks(), ticksLastFrame + FRAME_TARGET_TIME));
+	// Delta time is the difference in ticks from last frame converted to seconds
+	float deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f;
+
+	// Clamp deltaTime to a maximum value
+	deltaTime = (deltaTime > 0.05f) ? 0.05f : deltaTime;
+
+	// Sets the new ticks for the current frame to be used in the next pass
+	ticksLastFrame = SDL_GetTicks();
+
+	// Use deltaTime to update my game objects
+	projectilePos = glm::vec2(
+		projectilePos.x + projectileVel.x * deltaTime,
+		projectilePos.y + projectileVel.y * deltaTime
+	);
 }
 
 void Game::Render(){
@@ -75,8 +91,8 @@ void Game::Render(){
 	SDL_RenderClear(renderer);
 
 	SDL_Rect projectile {
-		(int) projectilePosX,
-		(int) projectilePosY,
+		(int) projectilePos.x,
+		(int) projectilePos.y,
 		10,
 		10
 	};
